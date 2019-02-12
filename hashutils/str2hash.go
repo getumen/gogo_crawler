c637e4d16e690c7dc845d7c76a9409c7781dde72
getumen <n.yoshihiro.jp@gmail.com>
2019-02-12 22:06:44 +0900

    rm hash log

--- hashutils/str2hash.go
+++ hashutils/str2hash.go
@@ -4,7 +4,6 @@ import (
 	"crypto/md5"
 	"encoding/base64"
 	"io"
-	"log"
 	"strings"
 )
 
@@ -14,6 +13,5 @@ func MakeHash(b []byte) string {
 		_, _ = io.WriteString(f, s)
 	}
 	s := base64.URLEncoding.EncodeToString(f.Sum(nil))
-	log.Println(s)
 	return s
 }
